2cef93c8933576f03ad3dbbfb77977e8e33784b9
bokuweb <bokuweb12@gmail.com>
2016-11-23 10:30:43 +0900

    Fix bug, the sprite value was 0 or 1

--- index.js
+++ index.js
@@ -10,28 +10,28 @@ const PROGRAM_ROM_SIZE = 0x4000;
 const CHARACTOR_ROM_SIZE = 0x2000;
 const DEFAULT_CANVAS_WIDTH = 800;
 
-const nesPath = process.argv[2];
-const distPath = process.argv[3] || 'sprite.png';
-const pixelSize = ~~(process.argv[4] || 1);
+const NES_PATH = process.argv[2];
+const DIST_PATH = process.argv[3] || 'sprite.png';
+const PIXEL_RATIO = ~~(process.argv[4] || 1);
 
 let nes;
 try {
-  nes = fs.readFileSync(nesPath);
+  nes = fs.readFileSync(NES_PATH);
 } catch (error) {
   throw new Error(error);
 }
 
 if ([].slice.call(nes, 0, 3).map(v => String.fromCharCode(v)).join('') !== 'NES') {
-  throw new Error(`${nesPath} is not an NES file.`);
+  throw new Error(`${NES_PATH} is not an NES file.`);
 }
 
 const programROMPages = nes[4];
 const characterROMPages = nes[5];
-const spritesPerRow = DEFAULT_CANVAS_WIDTH / (8 * pixelSize);
+const spritesPerRow = DEFAULT_CANVAS_WIDTH / (8 * PIXEL_RATIO);
 const spritesNum = CHARACTOR_ROM_SIZE * characterROMPages / 16;
 const rowNum = ~~(spritesNum / spritesPerRow) + 1;
 
-const height = rowNum * 8 * pixelSize;
+const height = rowNum * 8 * PIXEL_RATIO;
 const canvas = new Canvas(DEFAULT_CANVAS_WIDTH, height);
 const ctx = canvas.getContext('2d');
 
@@ -39,23 +39,33 @@ ctx.fillStyle = "rgb(0, 0, 0)";
 ctx.fillRect(0, 0, DEFAULT_CANVAS_WIDTH, height);
 
 const charactorROMsart = NES_HEADER_SIZE + programROMPages * PROGRAM_ROM_SIZE;
-const renderSprite = (spriteNum) => {
+const buildSprite = (spriteNum) => {
+  const sprite = Array.apply(null, Array(8)).map((_) => [0,0,0,0,0,0,0,0]);
   for (let i = 0; i < 16; i++) {
-    ctx.fillStyle = i > 7 ? "rgb(255, 255, 255)" : "rgb(128, 128, 128)";
     for (let j = 0; j < 8; j++) {
       if (nes[charactorROMsart + spriteNum * 16 + i] & (0x80 >> j )) {
-        const x = (j + (spriteNum % spritesPerRow) * 8) * pixelSize;
-        const y = ((i % 8) + ~~(spriteNum / spritesPerRow) * 8) * pixelSize;
-        ctx.fillRect(x, y, pixelSize, pixelSize);
+        sprite[i % 8][j] = sprite[i % 8][j] ? 3 : 1;
       }
+    }
+  }
+  return sprite;
+}
+const renderSprite = (sprite, spriteNum) => {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      ctx.fillStyle = `rgb(${85 * sprite[i][j]}, ${85 * sprite[i][j]}, ${85 * sprite[i][j]})`;
+      const x = (j + (spriteNum % spritesPerRow) * 8) * PIXEL_RATIO;
+      const y = (i + ~~(spriteNum / spritesPerRow) * 8) * PIXEL_RATIO;
+      ctx.fillRect(x, y, PIXEL_RATIO, PIXEL_RATIO);
     }
   }
 };
 
 for(let i = 0; i < spritesNum; i++) {
-  renderSprite(i);
+  const sprite =  buildSprite(i);
+  renderSprite(sprite, i);
 }
 
 const data = canvas.toDataURL().split(',')[1];
 const buffer = new Buffer(data, 'base64');
-fs.writeFileSync(distPath, buffer);
+fs.writeFileSync(DIST_PATH, buffer);
